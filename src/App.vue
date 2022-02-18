ef811d1b821985996a434074145f0d6c55f4a30a
FlipmakerDm <vip-dim@mail.ru>
2022-02-18 19:53:22 +0300

    add Bar

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <BarChart :value="value1" />
+    <br />
+    <BarChart :value="value2" />
+  </div>
+</template>
+
+<script>
+import BarChart from './components/BarChart.vue';
+export default {
+  name: 'App',
+  components: { BarChart },
+  data: () => ({
+    value1: [23, 3, 45, 300, 10, 1, 64, 64],
+    value2: [23, 3, 45, 10, 10, 1, 64, 88, 55, 89, 22, 44, 99],
+  }),
+  methods: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 1024px;
+  margin: 60px auto;
+}
+</style>
---
+++ src/components/BarChart.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="bar-chart">
+    <div v-for="(item, index) in computedValue" :key="index" class="bar" v-bind:style="item.style">{{ item.item }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BarChart',
+  props: {
+    value: Array,
+  },
+  computed: {
+    computedValue() {
+      const max = Math.max(...this.value);
+
+      return this.value.map((item) => {
+        return {
+          item,
+          style: {
+            width: `${100 / this.value.length - 2}%`,
+            height: `${(item * 100) / max}%`,
+          },
+        };
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.bar {
+  background: red;
+}
+.bar-chart {
+  width: 400px;
+  height: 200px;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-around;
+}
+</style>
